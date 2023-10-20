6bf4d5fa7e13ec3b2da8840f08043394ee24c5a6
João Dantas <joaogdantasdev@hotmail.com>
2023-10-20 00:56:02 -0300

    PATCH: Removed filters

--- src/main/java/com/projetoaps/SistemaHotelaria/infra/security/SecurityConfigurations.java
+++ src/main/java/com/projetoaps/SistemaHotelaria/infra/security/SecurityConfigurations.java
@@ -27,7 +27,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
